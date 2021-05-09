d8ead214fc6bfcf2440e8e651c0550e617b1810d
ZonePG <619586533@qq.com>
2021-05-09 23:47:16 +0800

    Changes: 1. main.c sbi.h   add muticore start
    2. gdbinit.py
      change display src and asm window

--- scripts/gdbinit.py
+++ scripts/gdbinit.py
@@ -4,7 +4,7 @@ def main():
     gdb.execute('target remote localhost:3008')
     gdb.execute('b main')
     gdb.execute('c')
-    gdb.execute('layout src')
+    gdb.execute('layout split')
 
 if __name__ == "__main__":
     main()
