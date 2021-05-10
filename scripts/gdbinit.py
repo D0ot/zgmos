84bc97ee8f6c1e746b504c3abf376699766f7a5c
doot <49939375+D0ot@users.noreply.github.com>
2021-05-11 01:05:08 +0800

    Revert "Changes:"

--- scripts/gdbinit.py
+++ scripts/gdbinit.py
@@ -4,7 +4,7 @@ def main():
     gdb.execute('target remote localhost:3008')
     gdb.execute('b main')
     gdb.execute('c')
-    gdb.execute('layout split')
+    gdb.execute('layout src')
 
 if __name__ == "__main__":
     main()
